0177ab766be23fa0d1b7626e1a82db7bec1d1afd
MIshkhan <martirosyan.1996@list.ru>
2017-08-04 15:25:32 +0400

    Update init.el

--- init.el
+++ init.el
@@ -4,48 +4,38 @@
 	(add-to-list 'package-archives '("marmalade" . "http://marmalade.ferrier.me.uk/packages/"))
 (package-initialize)
 
-;;lines number
-(global-linum-mode t)
 
-;;theme
-(setq monokai-background "#24292e")
-(load-theme 'monokai t)
+(setq monokai-background "#24292e") (load-theme 'monokai t)                                    ;;theme
+(add-to-list 'load-path "/home/ishkhan/.emacs.d/elpa/neotree-20170522.758") (require 'neotree) ;;neo-tree
 
-;;delete-selection-mode
-(delete-selection-mode 1)
+(defalias 'yes-or-no-p 'y-or-n-p)                                                              ;;yes-no alias
 
-;;auto-complete
-(global-auto-complete-mode t)
+;;modes
+(global-linum-mode t)               ;;lines number
+(delete-selection-mode 1)           ;;delete-selection
+(global-auto-complete-mode t)       ;;auto-complete
+(setq subword-mode 1)               ;;subword
+(setq-default indent-tabs-mode nil) ;; tabs
 
-;;neo-tree
-(add-to-list 'load-path "/home/ishkhan/.emacs.d/elpa/neotree-20170522.758")
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+(setq make-backup-files nil)        ;; stop creating backup~ files
+(setq auto-save-default nil)        ;; stop creating #autosave# files
 
 ;;all-the-icons		
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (custom-set-variables
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (json-mode scala-mode php-mode neotree monokai-theme highlight-symbol auto-complete all-the-icons))))
 (custom-set-faces)
 
-;;java-mode and hs-minore mode
-(add-hook 'java-mode-hook (lambda ()
-			    (hs-minor-mode 1)
-			    (local-set-key (kbd "<f1>") 'hs-toggle-hiding)
-			    (local-set-key (kbd "<f2>") 'hs-hide-all)
-			    (local-set-key (kbd "<f3>") 'hs-show-all)
-			    (setq c-basic-offset 2)))
+;;custom-set-keys
+(global-set-key [f1] 'hs-toggle-hiding)                ;;hide-show
+(global-set-key [f2] 'highlight-symbol-prev)           ;;highlight-prev-sysmbol
+(global-set-key [f3] 'highlight-symbol-at-point)       ;;highlight-all-sysmbols 
+(global-set-key [f4] 'highlight-symbol-next)           ;;highlight-next-sysmbol
 
-;;yes-no alias
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;;subword-mode
-(setq subword-mode 1)
-
-;; highlight-sysmbol configurations
-(global-set-key [f4] 'highlight-symbol-at-point)
-(global-set-key [(control f4)] 'highlight-symbol-next)
-(global-set-key [(shift f4)] 'highlight-symbol-prev)
-(lambda (event) (interactive "e") (save-excursion (goto-char (posn-point (event-start event)))(highlight-symbol-at-point)))
-
-
+(global-set-key [f5] (lambda() (interactive) (load-file "~/.emacs.d/init.el"))) ;;loading init.el
+(global-set-key [f6] (lambda() (interactive) (find-file "~/.emacs.d/init.el"))) ;;opening init.el
+(global-set-key [f7] 'find-file)      ;;finding file
+(global-set-key [f8] 'neotree-toggle) ;;neo-tree
